dbaf1f16d0126df7396dae3ef58f61b6bf89a209
danish1594 <aroradanish533@gmail.com>
2021-02-03 12:40:25 +0530

    first commit

---
+++ src/app/Pages/internet-billing/internet-billing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-internet-billing',
+  templateUrl: './internet-billing.component.html',
+  styleUrls: ['./internet-billing.component.css']
+})
+export class InternetBillingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InternetBillingComponent } from './Pages/internet-billing/internet-billing.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InternetBillingComponent,
   ],
   imports: [
     BrowserModule,
