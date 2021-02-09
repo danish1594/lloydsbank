21b5a8ae60ff69c598b6a8f0c8c4d3c2b324e91b
danish1594 <aroradanish533@gmail.com>
2021-02-09 22:00:46 +0530

    fourth page commit

---
+++ src/app/Pages/internet-billing/billing-data.ts
@@ -0,0 +1,47 @@
+export const TableData = {
+    cashISA: [
+        { time: '25/03/20 – current', balance: '£1+', AER: '0.05%', QualifyingCriteria: 'N/A' },
+        { time: '04/07/19 – 24/03/20', balance: '£1+', AER: '0.05%', QualifyingCriteria: 'N/A' },
+    ],
+
+    table2: [
+        {
+            heading: '21/07/20 – current',
+            data: [
+                {balance: '£1+', TaxFreeAER: '0.30%', TaxFree: '0.30%' , AER: '0.30%', QualifyingCriteria: 'N/A'},
+            ]
+        },
+        {
+            heading: '09/06/20 – 20/07/20',
+            data: [
+                { balance: '£1+', TaxFreeAER: '0.30%', TaxFree: '0.30%' , AER: '0.30%', QualifyingCriteria: 'N/A' },
+                { balance: '£1+' , TaxFreeAER: '0.40%', TaxFree: '0.40%' , AER: '0.40%', 
+                QualifyingCriteria: 'You already hold either a Club Lloyds personal current account or a Wealth personalcurrent account' },
+            ]
+        },
+        {
+            heading: '05/05/20 – 08/06/20',
+            data: [
+                { balance: '£1+', TaxFreeAER: '0.40%', TaxFree: '0.40%' , AER: '0.40%', QualifyingCriteria: 'N/A' },
+                { balance: '£1+', TaxFreeAER: '0.30%', TaxFree: '0.30%' , AER: '0.30%', QualifyingCriteria: 'N/A'  }
+            ],
+
+        },
+        {
+            heading: 'one',
+            data: [
+                { balance: '£1+', TaxFreeAER: '0.30%', TaxFree: '0.30%' , AER: '0.30%', QualifyingCriteria: 'N/A' },
+                { balance: '£1+', TaxFreeAER: '0.30%', TaxFree: '0.30' , AER: '0.30%', QualifyingCriteria: 'N/A'  }
+            ],
+
+        }
+    ],
+
+    ClubLloydsMonthlySaver: [{ time: '27/05/20 – 20/07/20', balance: '£1+', AER: '0.05%' },
+    { time: '27/05/20 – 20/07/20', balance: '£1+', AER: '2.0%' },
+    { time: '27/05/20 – 20/07/20', balance: '£1+', AER: '2.0%' },
+    { time: '27/05/20 – 20/07/20', balance: '£1+', AER: '2.0%' },
+    { time: '27/05/20 – 20/07/20', balance: '£1+', AER: '2.0%' },
+    { time: '27/05/20 – 20/07/20', balance: '£1+', AER: '2.0%' }
+    ]
+};
--- src/app/Pages/internet-billing/internet-billing.component.ts
+++ src/app/Pages/internet-billing/internet-billing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TableData } from './billing-data';
 
 @Component({
   selector: 'app-internet-billing',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InternetBillingComponent implements OnInit {
 
+  tableData = TableData;
   constructor() { }
 
   ngOnInit(): void {
